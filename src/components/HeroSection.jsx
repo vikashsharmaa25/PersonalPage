62a2a518a97f7e681d1a8009f03bbfe50f2b88f7
Vikash Sharma <Vikash9422@gmail.com>
2024-02-24 16:55:59 +0530

    resume

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -74,8 +74,7 @@ const HeroSection = () => {
                 Hire Me
               </ScrollLink>
               <a
-                // href="https://drive.google.com/file/d/1wEDlamry-sl0mYrE4tbxitZaHy-s7jSX/view?usp=drive_link"
-                href="/"
+                href="https://drive.google.com/file/d/198cQx1AxesC6YefD5t7YZewj57Kr41xh/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-6 py-3 w-full sm:w-fit rounded-full md:mr-4 bg-transparent border-white border-2 text-white text-center hover:bg-darkHover hover:scale-[0.99] transition-all duration-400 ease-in-out "
